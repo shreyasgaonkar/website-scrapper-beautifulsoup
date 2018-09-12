219d2a2e523394577e784ca2c055a98feaa2892a
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2018-09-11 23:07:20 -0500

    Update remitly.py

--- remitly.py
+++ remitly.py
@@ -1,16 +1,15 @@
-import re
 from bs4 import BeautifulSoup
 import requests
 from django.utils.encoding import smart_str, smart_unicode
+import re
 
 source = requests.get('https://www.remitly.com/us/en/india/pricing').text
 
 soup = BeautifulSoup(source, 'lxml')
 rate = soup.find('div')
 
-rate = rate.find('div', class_='f1a8qg93').td.text
-
-string = (soup.select("tbody.providerList_f1mjsvhw  td.f1cdddfp  div.feeValue_fcoij7e")[0].text)
+string = (soup.select(
+    "tbody.providerList_f1mjsvhw  td.f1cdddfp  div.feeValue_fcoij7e")[0].text)
 currentRate = re.findall(r'\d{2}.\d{2}', smart_str(string))
 
 print (currentRate)
