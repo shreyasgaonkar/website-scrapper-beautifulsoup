6c6c2fdca1516c76299a8910a4a543a8aeebefb0
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2018-09-22 09:56:23 -0500

    update code to include both rates

--- remitly.py
+++ remitly.py
@@ -7,9 +7,15 @@ source = requests.get('https://www.remitly.com/us/en/india/pricing').text
 
 soup = BeautifulSoup(source, 'lxml')
 rate = soup.find('div')
+try:
 
-string = (soup.select(
-    "tbody.providerList_f1mjsvhw  td.f1cdddfp  div.feeValue_fcoij7e")[0].text)
-currentRate = re.findall(r'\d{2}.\d{2}', smart_str(string))
+    express = (soup.select("tbody.providerList_f1mjsvhw > tr:nth-of-type(1) td.f1cdddfp  div.feeValue_fcoij7e")[0].text)
+    economy = (soup.select("tbody.providerList_f1mjsvhw > tr:nth-of-type(2) td.f1cdddfp  div.feeValue_fcoij7e")[0].text)   
 
-print (currentRate)
+    currentRateExpress = re.findall(r'\d{2}.\d{2}', smart_str(express))
+    currentRateEconomy = re.findall(r'\d{2}.\d{2}', smart_str(economy))
+
+    print("Current express rate: {}, economy rate: {}".format(currentRateExpress[0], currentRateEconomy[0])) 
+
+except:
+    print("Skipped, cannot find the requested rate on the website. Try again later.")
